79cf74c91f94b8f2fd0ef5bc071b87032a4d790d
MitchZas <mitchellzasa@gmail.com>
2024-12-16 21:51:46 -0500

    Coal is counted and deposited
    Coal can now be counted when collected and when deposited into the furnace, the correct text will appear.
    
    The next thing to do is to update the UI to fill according to how much coal was deposited

--- Assets/DepositCoal.cs
+++ Assets/DepositCoal.cs
@@ -5,11 +5,19 @@ public class DepositCoal : MonoBehaviour
 {
     public Canvas coalDepositCanvas;
     public GameObject cartCoal;
+    public GameObject collectCoal;
 
     public int minCoal = 0;
     public int currentCoal;
+    public int coalDeposited;
 
     private bool triggerEntered = false;
+    private DestroyCoal destroyCoalscript;
+
+    void Start()
+    {
+        destroyCoalscript = collectCoal.GetComponent<DestroyCoal>();
+    }
 
     void Update()
     {
@@ -37,10 +45,31 @@ public class DepositCoal : MonoBehaviour
     void CoalDeposit()
     {
         {
+            // If there are zero coal objects active, set the value to 0
+            if (DestroyCoal.coalNumber == 0)
+            {
+                Debug.Log("No Coal was deposited");
+            }
             // If there is one coal object active, set the value to 1
+            if (DestroyCoal.coalNumber == 1)
+            {
+                Debug.Log("One Coal was deposited");
+            }
             // If there are two coal objects active, set the value to 2
+            if (DestroyCoal.coalNumber == 2)
+            {
+                Debug.Log("Two Coal were deposited");
+            }
             // If there are three coal objects active, set the value to 3
+            if (DestroyCoal.coalNumber == 3)
+            {
+                Debug.Log("Three Coal were deposited");
+            }
             // If there are four coal objects active, set the value to 4
+            if (DestroyCoal.coalNumber == 4)
+            {
+                Debug.Log("Four Coal were deposited");
+            }
         }
     }
 
--- Assets/Scripts/DestroyCoal.cs
+++ Assets/Scripts/DestroyCoal.cs
@@ -1,16 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Destroy : MonoBehaviour
+public class DestroyCoal : MonoBehaviour
 {
     public GameObject coalAppear;
     public GameObject coalDisappear;
     public GameObject cart;
 
+    public static int coalNumber;
+
     private PlayerMovement playerMovementScript;
 
-    private void Start()
+    public bool destroyScript = false;
+
+    void Start()
     {
         playerMovementScript = cart.GetComponent<PlayerMovement>();
+        coalNumber = 0;
     }
 
 
@@ -19,12 +26,14 @@ public class Destroy : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             AddCoal();
+            Destroy(gameObject);
         }
     }
 
     public void AddCoal()
     {
-        coalDisappear.SetActive(false);
+        Debug.Log(coalNumber += 1);
+        //coalDisappear.SetActive(false);
         coalAppear.SetActive(true);
         playerMovementScript.jumpForce -= 2;
     }
