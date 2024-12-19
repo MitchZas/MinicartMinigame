08d8f275bc4ab5026daa9d6773708383a6058af9
MitchZas <mitchellzasa@gmail.com>
2024-12-19 09:32:54 -0500

    Color on UI wheel changes depending on how much coal you deposited

--- Assets/CalculateCoal.cs
+++ Assets/CalculateCoal.cs
@@ -3,15 +3,21 @@ using UnityEngine.UI;
 public class CalculateCoal : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
-    public void SetMaxCoal(int coal)
+    public void SetMinCoal(int coal)
     {
         slider.minValue = coal;
         slider.value = coal;
+
+        fill.color = gradient.Evaluate(0f);
     }
     
     public void SetCoal(int coal)
     {
         slider.value = coal;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
--- Assets/DepositCoal.cs
+++ Assets/DepositCoal.cs
@@ -11,12 +11,15 @@ public class DepositCoal : MonoBehaviour
     public int currentCoal;
     public int coalDeposited;
 
+    public CalculateCoal calculateCoal;
+
     private bool triggerEntered = false;
     private DestroyCoal destroyCoalscript;
 
     void Start()
     {
         destroyCoalscript = collectCoal.GetComponent<DestroyCoal>();
+        calculateCoal.SetMinCoal(minCoal);
     }
 
     void Update()
@@ -48,26 +51,31 @@ public class DepositCoal : MonoBehaviour
             // If there are zero coal objects active, set the value to 0
             if (DestroyCoal.coalNumber == 0)
             {
+                calculateCoal.SetCoal(0);
                 Debug.Log("No Coal was deposited");
             }
             // If there is one coal object active, set the value to 1
             if (DestroyCoal.coalNumber == 1)
             {
+                calculateCoal.SetCoal(1);
                 Debug.Log("One Coal was deposited");
             }
             // If there are two coal objects active, set the value to 2
             if (DestroyCoal.coalNumber == 2)
             {
+                calculateCoal.SetCoal(2);
                 Debug.Log("Two Coal were deposited");
             }
             // If there are three coal objects active, set the value to 3
             if (DestroyCoal.coalNumber == 3)
             {
+                calculateCoal.SetCoal(3);
                 Debug.Log("Three Coal were deposited");
             }
             // If there are four coal objects active, set the value to 4
             if (DestroyCoal.coalNumber == 4)
             {
+                calculateCoal.SetCoal(4);
                 Debug.Log("Four Coal were deposited");
             }
         }
